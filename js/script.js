2a7578a426ad08704efcff6f8fcc450ad725368f
Bernardo Maia Detomi <78918455+BernardoDetomi@users.noreply.github.com>
2024-04-23 18:10:28 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
